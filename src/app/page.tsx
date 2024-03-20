f05d71ce6d92b84fc45f19b16230211f9c54b185
heyolaniran <olaniran.abd@gmail.com>
2024-03-20 16:45:58 +0100

    feat: shadcnui & requirments completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import CollaborativeApp from "./CollaborativeApp";
 export default function page () {
   return (
     
-      <CollaborativeApp />
+      <>
+        <div className="min-h-screen w-full flex justify-center items-center text-center">
+
+          <h3 className="text-4xl"> Minimalist Figma  </h3> 
+          
+        </div>
+       
+      </>
+      
    
   )
 }
