07159eb13a86ab9734c3fbe5ea59e422e74ba653
heyolaniran <olaniran.abd@gmail.com>
2024-03-20 16:35:40 +0100

    feat: necessary ressources for app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Room } from "./Room";
 
 import CollaborativeApp from "./CollaborativeApp";
 
 export default function page () {
   return (
-    <Room>
+    
       <CollaborativeApp />
-    </Room>
+   
   )
 }
