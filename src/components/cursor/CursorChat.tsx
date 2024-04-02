2bc1c1c103ae87f64547c70c3355419e77968fa0
heyolaniran <olaniran.abd@gmail.com>
2024-04-02 17:56:57 +0100

    fix: cursor chat closing on key down e

--- src/components/cursor/CursorChat.tsx
+++ src/components/cursor/CursorChat.tsx
@@ -42,7 +42,9 @@ export default function CursorChat({cursor ,  cursorState , setCursorState , upd
 
                             <>
                                 <CursorSVG color="#000000" /> 
-                                <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+                                <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]"
+                                onKeyUp={(e)=> e.stopPropagation()}
+                                >
                                     {cursorState.previousMessage && (
                                         <div>
                                             {cursorState.previousMessage}
