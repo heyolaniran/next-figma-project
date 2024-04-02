a1a9e2017ab7e1feec28c170248213b8e631d127
heyolaniran <olaniran.abd@gmail.com>
2024-04-02 16:46:30 +0100

    feat: active users & navbar starting

---
+++ src/components/users/Avatar.tsx
@@ -0,0 +1,17 @@
+
+import styles from "./index.module.css";
+
+const IMAGE_SIZE = 48;
+
+export default function Avatar({ src, name }: { src: string; name: string }) {
+  return (
+    <div className={styles.avatar} data-tooltip={name}>
+      <img
+        src={src}
+        height={IMAGE_SIZE}
+        width={IMAGE_SIZE}
+        className={styles.avatar_picture}
+      />
+    </div>
+  );
+}
